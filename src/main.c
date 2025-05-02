01b37a7a5d9df5fdad596b83a92d9bc74ef2b357
NguyennHieuu <nguyenhieugp2005@gmail.com>
2025-05-02 20:13:51 +0700

    Update main.c
    dàn ý cho main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,23 @@
 #include<stdio.h>
+
+#include<book.h>
+#include<loan.h>
+#include<member.h>
+#include<utils.h>
+
 int main() {
-    printf("hello world");
+    menu();
+    int choice; // Lựa chọn của người dùng
+    while (choice != 0) {
+        printf("Your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1: /*Books option*/; break;
+            case 2: /*Readers option*/; break;
+            case 3: /*Mượn trả option*/; break;
+            case 4: /*Thống kế option*/; break;
+            case 0: /*Exit option*/; break;
+            default: printf("Lựa chọn không hợp lệ!\n");
+        }
+    }  
 }
