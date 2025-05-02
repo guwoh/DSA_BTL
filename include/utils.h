ef1bbee63095ef9e48f5a84a4cc29fca0d3f7847
NguyennHieuu <nguyenhieugp2005@gmail.com>
2025-05-02 20:19:13 +0700

    Update utils.h
    dàn ý file utils.h

---
+++ include/utils.h
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+#include<book.h>
+#include<loan.h>
+#include<member.h>
+// #include<struct.h>
+
+// HÀM liên quan đến thống kê
+// Hiển thị các loại thống kế
+void showStats();
+// Sách được mượn nhiều nhất
+//Book* mostBorrowedBook(); 
+// Độc giả mượn nhiều sách nhất
+//Reader* topReader(); 
+// Tổng số sách đang mượn 
+int booksBorrowed(); 
+
+// MENU
+void menu(); // Menu chính
