33341af8175430e3025d51a769d37396e9a34f6c
guwoh <[hoanghiep2k5@gmail.com]>
2025-05-08 15:17:27 +0700

    wsl test first time

--- src/main.c
+++ src/main.c
@@ -1,23 +1,43 @@
-#include<stdio.h>
+#include <stdio.h>
 
-#include<book.h>
-#include<loan.h>
-#include<member.h>
-#include<utils.h>
+#include "book.h"
+#include "loan.h"
+#include "member.h"
+#include "utils.h"
 
 int main() {
-    menu();
+    menu(); // Hàm này bạn phải định nghĩa trong utils.h hoặc tương tự
+	// hiep is here
     int choice; // Lựa chọn của người dùng
-    while (choice != 0) {
+    do {
         printf("Your choice: ");
         scanf("%d", &choice);
+
         switch (choice) {
-            case 1: /*Books option*/; break;
-            case 2: /*Readers option*/; break;
-            case 3: /*Mượn trả option*/; break;
-            case 4: /*Thống kế option*/; break;
-            case 0: /*Exit option*/; break;
-            default: printf("Lựa chọn không hợp lệ!\n");
+            case 1:
+                // Gọi hàm xử lý sách
+                handle_books();
+                break;
+            case 2:
+                // Gọi hàm xử lý người đọc
+                handle_readers();
+                break;
+            case 3:
+                // Gọi hàm xử lý mượn/trả
+                handle_loans();
+                break;
+            case 4:
+                // Gọi hàm thống kê
+                show_statistics();
+                break;
+            case 0:
+                printf("Thoát chương trình.\n");
+                break;
+            default:
+                printf("Lựa chọn không hợp lệ!\n");
         }
-    }  
+    } while (choice != 0);
+
+    return 0;
 }
+
