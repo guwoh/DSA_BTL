cec5b646ae692c6e0a3223d7296e9464e65b2bcc
Tpan <86758581+tpanackerman@users.noreply.github.com>
2025-05-28 07:57:46 +0700

    Update loan.h

--- include/loan.h
+++ include/loan.h
@@ -2,22 +2,33 @@
  * @brief  lưu trữ thông tin đến sách được mượn và người mượn sách 
 
  */
-#include<string.h>
+#include <string.h>
+#include <stdbool.h>
+
+#ifndef BORROW_H
+#define BORROW_H
+
+#define MAX_ID_LEN 20
+#define MAX_DATE_LEN 11
+
+typedef struct BorrowSlip {
+    char slipID[MAX_ID_LEN];
+    char bookID[MAX_ID_LEN];
+    char readerID[MAX_ID_LEN];
+    char borrowDate[MAX_DATE_LEN];
+    char dueDate[MAX_DATE_LEN];
+    char returnDate[MAX_DATE_LEN]; // nếu chưa trả thì để rỗng
+    struct BorrowSlip* next;
+} BorrowSlip;
+
+// Quản lý phiếu mượn
+void createBorrowSlip(BorrowSlip** head);
+void returnBook(BorrowSlip* head);
+void listBorrowSlips(BorrowSlip* head);
+void listUnreturnedBooks(BorrowSlip* head);
+void checkBorrowConditions(BorrowSlip* head);
+
+// Hàm kiểm thử
 void test_loan();
 
-typedef struct{
-    char slipID;
-    char bookID;
-    char readerID;
-    char borrowDate;
-    char dueDate;
-    char returnDate;
-    //bool isReturned;
-    BorrowSlip* next;
-}BorrowSlip;
-
-void createBorrowSlip();
-void returnBook();
-void listBorrowSlips();
-void listUnreturnedBooks();
-void checkBorrowConditions();
+#endif
