c748a6371d58944af5b333d43b3c992804f79c68
guwoh <hoanghiep2k5@gmail.com>
2025-05-28 16:12:51 +0700

    update io.cpp

--- src/io.cpp
+++ src/io.cpp
@@ -1,5 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
 #include "../include/book.h"
 #include "../include/loan.h"
 #include "../include/member.h"
-#include "../include/utils.h"
 #include "../include/io.h"
+
+
+// thêm sách vào đầu danh sách
+void addBookAtHead(bookNode** head, book books) // 
+{
+    bookNode* newNode = (bookNode*)malloc(sizeof(bookNode));
+    newNode->data = books;
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// thêm sách vào cuối danh sách 
+void addBookAtTail();
+
+// in danh sách
+void printBook();
+
+// xóa sách theo tiêu đề 
+void deleteBookByTitle();
+
+
+
+/*ra file*/
+// ra file text
+void writeText();
+
+// ra file bin
+void writeBin();
+
+
+/*vao file*/
+// vao file bin
+void readBin();
+
+
